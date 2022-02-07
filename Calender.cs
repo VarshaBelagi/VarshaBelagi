1818c9075d263811bfb33dd787d1b4bf708a7995
VarshaBelagi <99172119+VarshaBelagi@users.noreply.github.com>
2022-02-07 23:33:21 +0530

    Add files via upload

---
+++ Calender.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+
+{
+    class Calendarprogram
+    {
+        static int year = new int();
+        static int month = new int();
+        static int[,] calendar = new int[6, 7];
+        private static DateTime date;
+
+        static void Draw()
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("\t", month);
+            Console.WriteLine("MON TU WED THU FRI SAT SUN");
+            private DateTime date;
+
+    }
+
+    static void FillCalendar()
+    {
+        int date = DateTime.DaysInMonth(year, month);
+        int currentDay = 1;
+        var dayOfWeek = (int)date.dayOfWeek;
+        for (int i = 0; i < calendar.GetLength(0); i++)
+        {
+            for (int j = 0; j < calendar.GetLength(1) && currentDay - dayOfWeek + 1 <= date; j++)
+            {
+                if (i == 0 && month > j)
+                {
+                    calendar[i, j] = 0;
+                }
+                else
+                {
+                    calendar[i, j] = currentDay - dayOfWeek + 1;
+                    currentDay++;
+                }
+            }
+        }
+    }
+
+    static void DrawCalendar()
+    {
+        for (int i = 0; i < calendar.GetLength(0); i++)
+        {
+            for (int j = 0; j < calendar.GetLength(1); j++)
+            {
+                if (calendar[i, j] > 0)
+                {
+                    if (calendar[i, j] < 10)
+                    {
+                        Console.Write(" " + calendar[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(calendar[i, j] + " ");
+                    }
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine(" ");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the year:");
+        year = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the month : ");
+        month = Convert.ToInt32(Console.ReadLine());
+
+        Draw();
+        FillCalendar();
+        DrawCalendar();
+
+    }
+}
+}
+
+
+
